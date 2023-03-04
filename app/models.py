3902865edb40e8944e5e0784894058bcd97bbc72
Aly_Inj3ctor <alishahmughal12345+2@gmail.com>
2023-03-04 16:40:44 +0500

    add new files

--- app/models.py
+++ app/models.py
@@ -9,3 +9,21 @@ class Breakfasts(Base):
     name = Column(String)
     description = Column(String)
     price = Column(Integer)
+
+
+class Dinners(Base):
+    __tablename__ = "dinners"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Integer)
+
+
+class Lunchs(Base):
+    __tablename__ = "lunchs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Integer)
